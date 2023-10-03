56da65df1a74b5ca3087dd53423b9bd4b7a45502
Robin Fantino <robin.fantino.rf@gmail.com>
2023-10-03 10:48:58 +0200

    commit before push

---
+++ exoNode.js
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+console.log(chalk.red("Robin Fantino le plus beau des hommes"));
+console.log(chalk.bgGreen("Thomas est le plus doué en JS"));
+console.log(chalk.blueBright("Hélène et son pérroquet !"))
+console.log(chalk.yellow("Fred est le agé du groupe bleu"))
